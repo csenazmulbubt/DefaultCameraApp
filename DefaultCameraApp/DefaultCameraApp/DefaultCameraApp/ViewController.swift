8eea2b86d588b6a9aa00e13e2b3a28baf33f0f77
Nazmul Hoda <57939419+csenazmulbubt@users.noreply.github.com>
2023-02-02 21:07:28 +0600

    Add files via upload
    InitialCommit

---
+++ DefaultCameraApp/DefaultCameraApp/DefaultCameraApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DefaultCameraApp
+//
+//  Created by Nazmul on 31/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //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
+        //fsdfsdfsdfsdfsd
+    }
+
+
+}
+
